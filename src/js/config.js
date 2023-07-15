7779775f04959c84cfe78afa5f987363dec91945
Demian <demyan.korotchenko@gmail.com>
2023-07-15 20:59:54 +0200

    Summary_1

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+axios.defaults.headers.common['x-api-key'] =
+  'live_WfU35sW4GoFIy2gtPF0xU3gWNdbGZz97qy4KtwWrG9TWrzoKkAC6CJ47Nj1nHJf5';
+
+import Notiflix from 'notiflix';
+
+export const BASE_URL = 'https://api.thecatapi.com/v1';
