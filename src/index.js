4370c1f1c40867472d2bc1918045e2a6bfe5a2d4
Demian <demyan.korotchenko@gmail.com>
2023-07-18 18:28:53 +0200

    12345

--- src/index.js
+++ src/index.js
@@ -28,26 +28,115 @@
 // Для повідомлень використовуй бібліотеку notiflix.
 
 import axios from 'axios';
-axios.defaults.headers.common['key'] = API_KEY;
-//import Notiflix from 'notiflix';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
 const API_KEY = '38277598-05a082c915074d2caf7c5aa6f';
-const BASE_URL = 'https://pixabay.com/api/';
-const query = input.value;
+axios.defaults.headers.common['key'] = API_KEY;
 
-new URLSearchParams({
-  q: query,
-  image_type: 'photo',
-  orientation: 'horizontal',
-  safesearch: true,
-  page: 1,
-  per_page: 40,
-});
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
-  selectEl: document.querySelector('.breed-select'),
-  contentEl: document.querySelector('.cat-info'),
-  loaderEl: document.querySelector('.loader'),
-  errorEl: document.querySelector('.error'),
+    formBtnEl: document.querySelector('.search-form-btn'),
+    inputEl: document.querySelector('.search-form-input'),
+    gallery: document.querySelector('.gallery'),
+    btnLoadMore: document.querySelector('.load-more'),
 };
+refs.formBtnEl.addEventListener('submit', onSubmitClick);
+refs.btnLoadMore.classList.add('is-hidden');
+
+
+
+function onSubmitClick(evt) {
+    evt.preventDefault();
+
+  const { submitQuery } = evt.target.elements;
+  const search_query = submitQuery.value.trim().toLowerCase();
+
+  if (!search_query) {
+   clearPage();
+     Notify.info('Enter data to search!', notifyInit);
+
+     refs.searchInput.placeholder = 'What we are looking for?';
+    return search_query;
+  }
+
+  getPhotos()
+  .then(data =>  {
+    
+    const markup = createMarkup(data.hits);
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+   
+    return data;
+  })
+  .catch(err => {
+    console.log(err.massege);
+    
+  });
+}
+
+async function getPhotos(page = 1) {
+
+    const query = input.value;
+    const BASE_URL = 'https://pixabay.com/api/';
+    const params = new URLSearchParams({
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
+    });
+  return await axios(`${BASE_URL}?${params}`).then((response) => {
+    
+    // { status, data }
+    if (response.status !== 200) {
+      throw new Error(error);
+    }
+
+      return response.hint;
+      
+  });
+};
+
+  function createMarkup(arr) {
+    return arr
+      .map(
+        ({
+          tags,
+          webformatURL,
+          largeImageURL,
+          likes,
+          views,
+          comments,
+          downloads,
+        }) => {
+          return `
+              <a href='${largeImageURL}' class="card-link js-card-link">
+              <div class="photo-card">
+                <img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" />
+                <div class="info">
+                  <div class="info-item info-item-likes">
+                    <button type="button" class="circle"></button>
+                    <div class="box-likes"><b>Likes</b>
+                    <span id="value">${likes}</span>
+                    </div>
+                    
+                  </div>
+                  <p class="info-item">
+                    <b>Views</b>
+                    ${views}
+                  </p>
+                  <p class="info-item">
+                    <b>Comments</b>
+                    ${comments}
+                  </p>
+                  <p class="info-item">
+                    <b>Downloads</b>
+                    ${downloads}
+                  </p>
+                </div>
+              </div>
+              </a>`;
+        }
+      )
+      .join('');
+  }
